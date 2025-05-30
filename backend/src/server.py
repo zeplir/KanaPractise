399d313afbeb537a4f4e6953b3edfa4ce63e263d
Leo Katakalidis <leo.rydh@gmail.com>
2025-05-30 21:54:41 +0200

    Addeed support for kanji on backend and frontend (Fixes #1)

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request, Flask as fk
 from flask_cors import CORS
+from http import HTTPStatus
 from utility import get_path
 import random
 import logging
@@ -18,6 +19,19 @@ CORS(app) # Enable CORS for all routes
 def home():
     data = request.json
     amount = data.get('amount')
+    if not amount or amount < 1:
+        logger.debug("Faulty amount value: %s", amount)
+        return jsonify("invalid_request"), HTTPStatus.BAD_REQUEST
+    try:
+        path_to_json = get_path("kanji")
+    except FileNotFoundError:
+        logger.debug("Incorrect kana type was chosen")
+        return jsonify(error=""), HTTPStatus.INTERNAL_SERVER_ERROR
+
+    with open(path_to_json, 'r', encoding='utf-8') as f:
+        raw_data = json.load(f)
+        random.shuffle(raw_data)
+        return jsonify(raw_data[:int(amount)])
 
 @app.route("/kana", methods=['POST'])
 def kana():
@@ -26,24 +40,23 @@ def kana():
     kana_type = data.get('kana_type')
 
     if not amount or not kana_type:
-        logger.debug("Either amount or kana_type was incorrectly set")
-        return jsonify(error="invalid_request"), 400
+        logger.debug("Either amount (%s) or kana_type (%s) was incorrectly set", amount, kana_type)
+        return jsonify(error="invalid_request"), HTTPStatus.BAD_REQUEST
 
     if int(amount) < 0:
         logger.debug("Amount was less than 0")
-        return jsonify(error="invalid_request"), 400
+        return jsonify(error="invalid_request"), HTTPStatus.BAD_REQUEST
 
     try:
         path_to_json = get_path(f"{kana_type}")
-        logger.debug("%s", path_to_json)
     except FileNotFoundError:
         logger.debug("Incorrect kana type was chosen")
-        return jsonify(error="invalid_request"), 400
+        return jsonify(error="invalid_request"), HTTPStatus.BAD_REQUEST
 
     with open(path_to_json, 'r', encoding='utf-8') as f:
         raw_data = json.load(f)
         random.shuffle(raw_data)
-        return jsonify(raw_data[:int(amount)])
+        return jsonify(raw_data[:int(amount)]), HTTPStatus.OK
 
 # Uses default ip: http://127.0.0.1:5000
 if __name__ == '__main__':
--- frontend/src/test.js
+++ frontend/src/test.js
@@ -1,22 +1,61 @@
-function request_kana() {
+// Fetches data for either katakana or kanji
+async function request_kana(amount, kana_type) {
     const url = "http://127.0.0.1:5000/kana";
     const headers = { "Content-Type": "application/json" };
     const body = JSON.stringify({
-        "amount": "10",
-        "kana_type": "hiragana",
+        "amount": amount,
+        "kana_type": kana_type,
     });
-    fetch(url, {
-        method: "POST",
-        headers: headers,
-        body: body
-    })
-        .then(response => response.json())
-        .then(data => console.log("Success:", data))
-        .catch(error => console.error("Error: ", error));
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: headers,
+            body: body
+        });
+        const data = await response.json();
+        console.log("Success:", data);
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
-const button = document.getElementById('sendBtn');
+// Works like request_kana, but seperated for debugging and
+// readablitys sake
+async function request_kanji(amount) {
+    const url = "http://127.0.0.1:5000/kanji";
+    const headers = { "Content-Type": "application/json" };
+    const body = JSON.stringify({"amount": amount});
 
-button.addEventListener('click', () => {
-    request_kana()
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: headers,
+            body: body
+        });
+        const data = await response.json();
+        console.log("Success:", data);
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+function handle_kana_data(data, game_mode) {
+    const test = document.getElementById("test");
+    test.innerHTML = data[0][game_mode]
+}
+
+const button = document.getElementById('start');
+
+button.addEventListener('click', async () => {
+    const amount = document.getElementById("amount");
+    const game_mode = document.getElementById("picked_mode");
+    if(game_mode != "kanji") {
+        data = await request_kana(amount.value, game_mode.value);
+        handle_kana_data(data, game_mode.value);
+    } else {
+        data = await request_kana(amount.value);
+    }
 })
